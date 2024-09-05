fe988b9a41b2351aa2fe16b6718d4a3db5ae7bf9
kevser özeren <ozerenkevser92@gmail.com>
2024-09-05 15:24:34 +0300

    Apply static rendering, import not found function

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,10 +1,29 @@
+import { notFound } from "next/navigation"
+
+// enable to create a page which was not pre-built at the request time
+export const dynamicParams = true
+
+// static rendering to pre-render each ticket page at a build time
+export async function getStaticParams() {
+  const ticketsRes = await fetch("http://localhost:8000/tickets")
+  
+  const tickets = await ticketsRes.json()
+
+  return tickets.map((ticket) => ({
+    id: ticket.id
+  }))
+}
+
 async function fetchTicket (id) {
   const ticketPromisedData = await fetch(`http://localhost:8000/tickets/${id}`, {
     next: {
       revalidate: 60
     }
   })
-  
+  // render 404 page if the page we are looking for does not exist
+  if(!ticketPromisedData.ok) {
+    notFound()
+  }
   return ticketPromisedData.json()
 }
 
